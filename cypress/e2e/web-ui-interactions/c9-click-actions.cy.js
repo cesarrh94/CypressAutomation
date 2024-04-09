ab6bd9717521548979996320a5ae4a33c7746bb4
cesar rodriguez <94cesarrh@gmail.com>
2024-04-09 12:52:06 -0600

    click action & options

--- cypress/e2e/web-ui-interactions/c9-click-actions.cy.js
+++ cypress/e2e/web-ui-interactions/c9-click-actions.cy.js
@@ -6,12 +6,31 @@ context('click actions demo', () => {
         cy.visit('https://www.letskodeit.com/practice')
 
         // default: click on the center of the element
+        /* cy.get('#opentab').click()
+        cy.get('#opentab').click('left')
+        cy.get('#opentab').click('right')
+        cy.get('#opentab').click('top')
+        cy.get('#opentab').click('topLeft')
+        cy.get('#opentab').click('topRight')
+        cy.get('#opentab').click('bottom')
+        cy.get('#opentab').click('bottomLeft')
+        cy.get('#opentab').click('bottomRight') */
 
         // .click() accepts an x and y coordinates
+        /* cy.get('#opentab').click(2, 3)
+        // double click on different coordinates
+        cy.get('#opentab').click(2, 3).click(10, 25) */
 
         // ignore checking error
+        /* cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').click()
+        cy.get('#displayed-text').click({ force: true }) */
+
+        // click on multiple elements
+        cy.get('input[type="checkbox"][name="cars"]').click({ multiple: true })
 
         // scroll behavior
+        cy.get('#mousehover').click({ scrollBehavior: 'top' })
 
     })
 })
